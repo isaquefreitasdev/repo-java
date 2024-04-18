cd60ff36656f823a5f4fdfba2c87ec901330358e
isaquefreitasdev <freitasmelo.mkt.online@gmail.com>
2024-04-18 19:52:29 -0300

    entrada de valores no vetor - exercicio9

--- src/vetorExercicio9/Main.java
+++ src/vetorExercicio9/Main.java
@@ -8,6 +8,20 @@ public class Main {
         int n = sc.nextInt();
         String[] name = new String[n];
         double[] age = new double[n];
-        double[] altura = new double[n];
+        double[] height = new double[n];
+        // Entrada de valores no Vetor;
+        for (int i = 0; i < n; i++) {
+            sc.nextLine();
+            System.out.println("Nome:");
+            String nome = sc.nextLine();
+            name[i] = nome;
+            System.out.println("Idade:");
+            double idade = sc.nextDouble();
+            age[i] = idade;
+            System.out.println("Altura:");
+            double altura = sc.nextDouble();
+            height[i] = altura;
+        }
+
     }
 }
