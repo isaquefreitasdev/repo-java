6d72a02ddf9f6c86a67e7e8761a876161ca86337
isaquefreitasdev <freitasmelo.mkt.online@gmail.com>
2024-04-18 19:44:46 -0300

    inicialização das variaveis principais

---
+++ src/vetorExercicio9/Main.java
@@ -0,0 +1,13 @@
+package vetorExercicio9;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] name = new String[n];
+        double[] age = new double[n];
+        double[] altura = new double[n];
+    }
+}
