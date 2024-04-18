b3f75a3b3ce545b826af0a64d64018ec1ed9ad57
isaquefreitasdev <freitasmelo.mkt.online@gmail.com>
2024-04-18 09:53:23 -0300

    Exercicio8 finalizado - bem desafiador e proveitoso

--- src/vetorExercicio8/Main.java
+++ src/vetorExercicio8/Main.java
@@ -5,14 +5,34 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Digite quantos valores serao adicionados ao vetor:");
         int n = sc.nextInt();
         int[] N = new int[n];
-        System.out.println("Digite quantos valores serao adicionados ao vetor:");
         // Entrada de valores no vetor
         for (int i = 0; i < n; i++) {
             sc.nextLine();
             int valores = sc.nextInt();
             N[i] = valores;
         }
+        // Soma dos VALORES e verificação se é par
+        int soma = 0;
+        int media = 0;
+        int qtdPares = 0;
+        boolean verifique = false;
+        for (int i = 0; i < n; i++) {
+
+            if (N[i] % 2 == 0) {
+                soma += N[i];
+                verifique = true;
+                qtdPares++;
+            }
+        }
+        if (verifique == true) {
+            media = soma / qtdPares;
+            System.out.println(media);
+        } else {
+            System.out.println("Nenhum par foi encontrado.");
+        }
+
     }
 }
